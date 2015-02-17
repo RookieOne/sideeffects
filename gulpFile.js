c3f53c97a5fcd012da2d64e5a88d425152988230
Jonathan Birkholz <rookieone@gmail.com>
2015-02-17 12:05:45 -0600

    updated gitignore

--- gulpFile.js
+++ gulpFile.js
@@ -1,16 +1,43 @@
 var gulp = require("gulp")
+var watchify = require('watchify')
+var browserify = require('browserify')
+var livereload = require('gulp-livereload')
+var to5ify = require("6to5ify")
+var reactify = require("reactify")
+var source = require("vinyl-source-stream")
+var rename = require("gulp-rename")
 
+var DEST = "build"
 var src = {
   files: "app/*.html"
 }
 
 gulp.task("default", ["watch"])
 
-gulp.task("watch", ["move-files"], function() {
+gulp.task("watch", ["move-files", "app"], function() {
   gulp.watch(src.files, ["move-files"])
 })
 
 gulp.task("move-files", function() {
   return gulp.src(src.files)
     .pipe(gulp.dest("build"))
+})
+
+gulp.task("app", function() {
+  var bundle = function() {
+    return bundler
+      .bundle()
+      .pipe(source("./app/routes.js"))
+      .pipe(rename("main.js"))
+      .pipe(gulp.dest("./build"))
+      .pipe(livereload())
+  }
+
+  var bundler = browserify("./app/routes.js", watchify.args)
+  bundler.transform(to5ify)
+  bundler.transform(reactify)
+  bundler.on("update", bundle)
+  bundler = watchify(bundler)
+
+  return bundle()
 })
--- server.js
+++ server.js
@@ -1,23 +1,21 @@
-var app = require('http').createServer(handler)
-var io = require('socket.io')(app);
-var fs = require('fs');
+var connect = require("connect")
+var serveStatic = require("serve-static")
 
-app.listen(8000);
+var app = connect()
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/build/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
+var logger = function(req, res, next) {
+  console.log("%s %s", req.method, req.url)
+  next()
 }
 
+app.use(logger)
+app.use(serveStatic(__dirname + "/build"))
+app.listen(3000)
+
+var io = require('socket.io')(3001, {})
+
 io.on('connection', function (socket) {
+  console.log("connect")
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
